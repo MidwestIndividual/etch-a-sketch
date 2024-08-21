41679d5b8ac55cce827580d4504c5f98bd87ea1e
Dominic.Fryman <fryman.dominic@gmail.com>
2024-08-21 08:37:17 -0400

    Add getColor(color) function, no functionality

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,20 +1,6 @@
-let mainGrid = document.querySelector(".main-grid");
-
-for (let i =0; i < 16; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.setAttribute("class","pixel");
-    newDiv.style.flexBasis = "25%";
-
-    mainGrid.appendChild(newDiv);
-}
-
 function changeColor(target){
     target.style.background = "pink";
 }
-
-mainGrid.addEventListener("mouseover", (event) => {
-    changeColor(event.target);
-})
 
 function clearGrid() {
     existingDivs = document.querySelectorAll(".pixel");
@@ -35,17 +21,35 @@ function generateSquares(numberPerSide) {
     }
 }
 
+function getColor(previousColor){
+    pass;
+}
+
+let mainGrid = document.querySelector(".main-grid");
+
+for (let i =0; i < 16; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class","pixel");
+    newDiv.style.flexBasis = "25%";
+
+    mainGrid.appendChild(newDiv);
+}
+
+mainGrid.addEventListener("mouseover", (event) => {
+    changeColor(event.target);
+})
+
 let button = document.querySelector(".change-button");
 button.addEventListener("click", (event) => {
-    let numberSides = +prompt("How many squares per side would you like? (Max 10)");
+    let numberSides = +prompt("How many squares per side would you like? (Max 100)");
 
     // Check for invalid input
     if (!(Number.isInteger(numberSides))){
         alert("Invalid input: Number must be an integer. Please try again!")
         return;
     }
-    if (!(numberSides > 0 && numberSides < 11)){
-        alert("Invalid input: Number must be between 1 and 10. Please try again!")
+    if (!(numberSides > 0 && numberSides < 101)){
+        alert("Invalid input: Number must be between 1 and 100. Please try again!")
         return;
     }
 
