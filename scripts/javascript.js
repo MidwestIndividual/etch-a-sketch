e867df5c1753bb15bb21231df9ff7ca4104858de
Dominic.Fryman <fryman.dominic@gmail.com>
2024-08-21 07:20:46 -0400

    Add basic logic to add pixels to gird

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,1 +1,10 @@
 console.log('Hello World');
+
+let mainGrid = document.querySelector(".main-grid");
+
+for (let i =0; i < 16; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class","pixel");
+
+    mainGrid.appendChild(newDiv);
+}
