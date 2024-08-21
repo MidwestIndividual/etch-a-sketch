91248229ba573c8d984583e8e88d9cffdc1c7706
Dominic.Fryman <fryman.dominic@gmail.com>
2024-08-21 07:29:23 -0400

    Lay out divs in a grid

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -5,6 +5,12 @@ let mainGrid = document.querySelector(".main-grid");
 for (let i =0; i < 16; i++) {
     let newDiv = document.createElement("div");
     newDiv.setAttribute("class","pixel");
+    //newDiv.setAttribute("width", "25%");
+    //newDiv.setAttribute("aspect-ratio", "1");
 
     mainGrid.appendChild(newDiv);
+}
+
+function changeColor(target){
+    target.setAttribute("color", "pink");
 }
